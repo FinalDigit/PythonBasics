114dc2cdc01d742be35ace4a7170fac5f675220e
FinalDigit <44064726+FinalDigit@users.noreply.github.com>
2018-10-11 08:38:04 -0700

    Create basics.py

---
+++ basics.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python  # Will use this Environment Path for Python version
+
+# Here is an example of constructing strings
+var = 'Hello World'
+var1 = "Hello World with Double Quotes"
+
+# Here is How to Print a Variable
+print(var)
+print(var1)
+
+# Here we will combine the string
+stringCombo = (var + ' ' + var1)
+print(stringCombo)
